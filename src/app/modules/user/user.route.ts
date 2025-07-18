cc56fd1446e7176b8177dbb597f332925ec0b42b
tahminatanjidamiti <mity2027@gmail.com>
2025-07-19 02:38:23 +0600

    part-1 code added for the backend project

---
+++ src/app/modules/user/user.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { UserControllers } from "./user.controller";
+
+
+const router = Router()
+
+router.post("/register", UserControllers.createUser)
+router.get("/all-users", UserControllers.getAllUsers)
+
+
+export const UserRoutes = router
