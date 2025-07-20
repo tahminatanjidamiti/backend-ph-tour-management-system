13308dafe410ed89f87387e64577bf6cff10cd0c
tahminatanjidamiti <mity2027@gmail.com>
2025-07-21 05:19:07 +0600

    part-3 code added for the project

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
+import cookieParser from "cookie-parser";
 import express, { Request, Response } from 'express';
 import cors from "cors"
 import { router } from './app/routes';
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import passport from "passport";
+import expressSession from "express-session";
+import "./app/config/passport"
+import { envVars } from "./app/config/env";
 
 
 const app = express()
 
+app.use(expressSession({
+  secret: envVars.EXPRESS_SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(cookieParser())
 app.use(express.json())
 app.use(cors())
 
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -7,14 +7,21 @@ interface EnvConfig {
     DB_URL: string,
     NODE_ENV: "development" | "production",
     BCRYPT_SALT_ROUND: string,
-    JWT_ACCESS_EXPIRES: string,
     JWT_ACCESS_SECRET: string,
+    JWT_ACCESS_EXPIRES: string,
+    JWT_REFRESH_SECRET: string,
+    JWT_REFRESH_EXPIRES: string,
     SUPER_ADMIN_EMAIL: string,
     SUPER_ADMIN_PASSWORD: string,
+    GOOGLE_CLIENT_SECRET: string,
+    GOOGLE_CLIENT_ID: string,
+    GOOGLE_CALLBACK_URL: string,
+    EXPRESS_SESSION_SECRET: string,
+    FRONTEND_URL: string
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD"];
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD", "GOOGLE_CLIENT_SECRET", "GOOGLE_CLIENT_ID", "GOOGLE_CALLBACK_URL", "EXPRESS_SESSION_SECRET", "FRONTEND_URL"];
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
             throw new Error(`Missing require environment variable ${key}`)
@@ -25,10 +32,17 @@ const loadEnvVariables = (): EnvConfig => {
         DB_URL: process.env.DB_URL as string,
         NODE_ENV: process.env.NODE_ENV as "development" | "production",
         BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
-        JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
         JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+        JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+        JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+        JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
         SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
         SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+        GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
+        EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
+        FRONTEND_URL: process.env.FRONTEND_URL as string,
     }
 
 }
